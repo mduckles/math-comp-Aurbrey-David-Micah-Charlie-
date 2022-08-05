227c5e2a4ca81ba8294a4ce84255b457ccfa4ffa
CubingBadWolf <gaming1dragons@gmail.com>
2022-08-06 10:27:29 +1200

    Added Slingshot Class

--- model.py
+++ model.py
@@ -3,4 +3,16 @@ import matplotlib.pyplot as plt
 import time
 import statistics
 
-#Model
+def ElasticEnergy(k,dx):
+    return (k*(dx)**2)/2
+
+Payload_Weight = 1
+class SlingShot():
+    def __init__(self, Spring, Size, Angle):
+        self.Spring = Spring 
+        self.Size = Size
+        self.Angle = Angle
+
+    def kineticOutcome(self):
+        return ElasticEnergy(self.Spring, self.Size)
+
