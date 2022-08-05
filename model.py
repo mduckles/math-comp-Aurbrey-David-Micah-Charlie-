19de39c626a34a2cf28d236f518f2fde2952b16c
CubingBadWolf <gaming1dragons@gmail.com>
2022-08-06 11:08:21 +1200

    Add force outcome

--- model.py
+++ model.py
@@ -15,24 +15,15 @@ class SlingShot():
         self.Size = Size
         self.Angle = Angle
 
-<<<<<<< HEAD
     def EnergyOutcome(self):
         '''Determines the ElasticPotential Energy of the slingshot at its maximum pulled distanc'''
-=======
-    def kineticOutcome(self):
-        '''out puts the ElasticEnergy'''
->>>>>>> 2b85c80d80327e6fe7bbcfa39ae1435fd037df78
         maxPullConst = 3*(11*0.5) # Proportion between length of elastic and the distanc pulled. 
         DistancePulled = maxPullConst/self.Size
         return ElasticEnergy(self.Spring, DistancePulled)
     
-<<<<<<< HEAD
     def ForceOutcome(self):
         '''Determines the Force of the object when it is pulled back in the slingshot'''
         return (2*(self.Spring * 4.5 * math.degrees(math.cos(math.asin(1/10)))))
-=======
-
->>>>>>> 2b85c80d80327e6fe7bbcfa39ae1435fd037df78
 
 Test1 = SlingShot(5, 10, 70)
 print(Test1.kineticOutcome())
