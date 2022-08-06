db17ed93f9597257e126f2d6168139aeb6e03fba
CubingBadWolf <gaming1dragons@gmail.com>
2022-08-06 15:33:35 +1200

    Removed unneccesary code + added further documentation

--- model.py
+++ model.py
@@ -1,48 +1,45 @@
-from re import M
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import math
 import statistics
-
-def ElasticEnergy(k,dx):
-    return (k*(dx)**2)/2
-
+#Importing any modules we may need
 
 class SlingShot():
+    '''The class of our slingshot object. Contains the models for calculating our mass'''
     def __init__(self, Spring, Size, Angle):
         '''Spring is the spring constant of the Elastic part. Size is the length of the elastic. Angle is the angle of release'''
         self.Spring = Spring 
         self.Size = Size
         self.Angle = Angle
-
-    def EnergyOutcome(self):
-        '''Determines the ElasticPotential Energy of the slingshot at its maximum pulled distanc'''
-        maxPullConst = (99**0.5)/2 # Proportion between length of elastic and the distanc pulled. 
-        DistancePulled = self.Size/maxPullConst
-        return ElasticEnergy(self.Spring, DistancePulled)
-    
-    def ForceOutcome(self):
-        '''Determines the Force of the object when it is pulled back in the slingshot'''
-        return (2*(self.Spring * 4.5 * math.degrees(math.cos(math.asin(1/10)))))
     
     def VelocityLaunch(self, mass):
-        '''Assuming velocity is linear '''
+        '''A model for calculating the velocity of the object based on the calculations done on paper'''
         Vel = (((99**0.5)**0.5) * (4.477**0.5) * (self.Spring**0.5) * self.Size)/ (mass**0.5)
         return Vel
 
     def MassOfVel(self, TargetVel):
-        return (99**0.5) * 4.477 * self.Spring * (self.Size**2) / TargetVel**2
+        '''Rearranged value of the previous model that we can plug in the escape velocity'''
+        return (99**0.5) * 4.4775 * self.Spring * (self.Size**2) / TargetVel**2
 
 
 
 escapeVel = 11200
+# The escape velocity in m/s
 size = 828/((99**0.5)/2)
+# The length of the elastic band
 BigSlingshot = SlingShot(81.75 * size/0.24, size, 70)
+# Imputting the information into the object
 Mass = BigSlingshot.MassOfVel(escapeVel)
+# Calculating the mass based on the escape velocity
 print(f'The mass of our object is {Mass} when the length of the elastic slingshot is {round(size, 3)}, when fired stright up ')
 
+'''70 Degree Launch'''
+
 size70deg = 828/((99**0.5)/2 * math.sin(math.radians(70)))
+# The maximum length of the elastic band when pulled at a 70 degree angle
 BigAngledSlingshot = SlingShot(81.75 * size70deg/0.24, size70deg, 70)
+# Imputting the new information into the object
 Mass70deg = BigAngledSlingshot.MassOfVel(escapeVel)
-print(f'The mass of our object is {Mass70deg} when the length of the elastic slingshot is {round(size70deg, 3)}, when fired at an angle of 70 degrees ')
+# Calculating the mass of the object when fired at 70 degrees from our model
+print(f'The mass of our object is {Mass70deg} when the length of the elastic slingshot is {round(size70deg, 3)}, when fired at an angle of 70 degrees')
